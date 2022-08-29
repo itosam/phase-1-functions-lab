4c2c16aeefbb12ca1bc2f5ed1c81b41b1e9afc16
Osamu Ito <mr.samito@gmail.com>
2022-08-29 12:07:31 -0400

    commit tree 8a5e24ec130b7b9b73c475d927914973f69f4b42

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+if ((blocks) < 42) {
+    return (42-blocks);
+} else {
+    return (blocks-42);
+}
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(x,y) {
+    if (x >y) {
+        return ((x-y)*264);
+    } else {
+        return ((y-x) *264);
+    }
+}
+
+function calculatesFarePrice(x,y) {
+let feet = distanceTravelledInFeet(x,y);
+
+if (feet <= 400) {
+    return 0
+} else if (feet > 400 && feet <= 2000 ) {
+    return (feet-400)*.02
+} else if (feet > 2000 && feet <= 2500) {
+    return 25
+}
+else ( feet > 2500)
+return "cannot travel that far";
+}
